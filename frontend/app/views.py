3e63856c0bd988a04d438ec32720386a1da8462c
Yanlin Liu <962205792@qq.com>
2019-12-11 22:55:19 -0500

    [UPDATE]

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render,render_to_response
 from django import forms
 from django.http import HttpResponse
 from app.models import Video
+from django.core.files.storage import FileSystemStorage
+import requests
+import os
+import json
 # Create your views here.
 class VideoForm(forms.Form):
     #file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     videoname = forms.CharField()    #string
     Frame = forms.FileField()
     File = forms.FileField()     #file
+
+
+url='http://127.0.0.1:5000/api/infer'
+
 
 def homepage(request):
 
@@ -25,8 +33,17 @@ def upload(request):
             video.videoname = videoname
             video.Frame = Frame
             video.File = File
-            video.save()
-            return render_to_response('video.html')
+            result = video
+            result.save()
+            x = requests.post(url,data={"videoname":videoname},files=request.FILES)
+            with open(os.path.join("app/static/video", videoname+".mp4"),"wb") as f:
+                f.write(x.content)
+            with open(os.path.join("app/static/headers", videoname+".json"),"w") as f:
+                f.write(json.dumps(dict(x.headers)))
+            v = os.path.join("/static/video", videoname+".mp4")
+            h = os.path.join("/static/headers", videoname + ".json")
+            x={"v":v,"h":h}
+            return render_to_response('video.html',x)
     else:
         uf = VideoForm()
 
@@ -36,3 +53,4 @@ def upload(request):
 def video(request):
 
     return render_to_response('video.html')
+
